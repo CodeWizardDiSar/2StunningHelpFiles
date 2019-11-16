0f3ceecfd30505991544b96d701ce517712b1296
CodeWizardDiSar <saridakisbitos@gmail.com>
2019-11-16 23:51:07 +0200

    m

---
+++ 1_Scripts/ranger.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SHELL=~/Desktop/2_Github/stunningHelpFiles/1_Scripts/rangerAliases.sh
+ranger --choosedir=$HOME/.rangerdir
---
+++ 2_templateFiles/3.hs
@@ -0,0 +1,9 @@
+--module _ where
+--import _._
+
+--_ _ = _
+
+--data _ = _ _ | _ _
+--type _ = _ 
+
+main = _
