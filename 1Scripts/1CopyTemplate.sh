e85a46f46de81aa180ae7cb4189a61a6d1be2ef8
CodeWizardDiSar <saridakisbitos@gmail.com>
2020-01-09 00:34:41 +0200

    m

--- 1Scripts/1CopyTemplate.sh
+++ 1Scripts/1CopyTemplate.sh
@@ -5,9 +5,10 @@ templatesDir="$scriptDir/../2Templates"
 case $1 in
   Makefile) cp $templatesDir/1Files/4Makefile $1;;
   *) case "${1##*.}" in
-       c) cp $templatesDir/1Files/1.c $1;vim $1;;
-       sh) cp $templatesDir/1Files/2.sh $1;chmod 700 $1;vim $1;;
-       hs) cp $templatesDir/1Files/3.hs $1;vim $1;;
+       c)   cp $templatesDir/1Files/1.c $1;vim $1;;
+       sh)  cp $templatesDir/1Files/2.sh $1;chmod 700 $1;vim $1;;
+       hs)  cp $templatesDir/1Files/3.hs $1;vim $1;;
+       py)  cp $templatesDir/1Files/4.py $1;chmod 700 $1;vim $1;;
        ltx) cp -r $templatesDir/2Dirs/1Latex $1; vim $1/1Code/1.tex;;
        *) echo "template for the extention ${1##*.} not supported"
      esac
