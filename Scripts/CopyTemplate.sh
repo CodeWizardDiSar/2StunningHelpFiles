d13da3140dd41f3459f3ccbbaf6be210fd46da54
gnostis <saridakisbitos@gmail.com>
2021-02-16 08:54:32 +0200

    I am an automated comment

--- Scripts/CopyTemplate.sh
+++ Scripts/CopyTemplate.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cd ../Templates
+tmps="$Stunning/StunningHelpFiles/Templates"
+
 case $1 in
   Makefile) cp Files/5Makefile $1;;
   *) case "${1##*.}" in
-       c)   cp Files/1.c $1;vim $1;;
-       sh)  cp Files/2.sh $1;chmod 700 $1;vim $1;;
-       hs)  cp Files/3.hs $1;vim $1;;
-       py)  cp Files/4.py $1;chmod 700 $1;vim $1;;
-       ltx) cp -r Directories/Latex $1; vim $1/Code/code.tex;;
+       c)   cp $tmps/Files/1.c $1;vim $1;;
+       sh)  cp $tmps/Files/2.sh $1;chmod 700 $1;vim $1;;
+       hs)  cp $tmps/Files/3.hs $1;vim $1;;
+       py)  cp $tmps/Files/4.py $1;chmod 700 $1;vim $1;;
+       ltx) cp -r $tmps/Dirs/Latex $1; vim $1/Code/code.tex;;
        *) echo "template for the extention ${1##*.} not supported"
      esac
 esac
